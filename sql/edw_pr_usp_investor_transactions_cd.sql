cc82f1799647a3ee79d89910ab32ef80cd1dc578
vphilips <venupgeorge@gmail.com>
2026-02-01 15:21:27 -0500

    Update edw_pr_usp_investor_transactions_cd.sql
    sql fix

--- sql/edw_pr_usp_investor_transactions_cd.sql
+++ sql/edw_pr_usp_investor_transactions_cd.sql
@@ -62,88 +62,72 @@ fundtype_exclude AS
 SELECT TOP (@EffectiveMaxRows)
     f.Metric_ID                          AS metric_id,
     @MetricName                          AS metric_name,
-    f.investor_name_id                   AS investor_name_id,
+    NULL                                 AS investor_name_id, -- Not directly available in valuation table?
 
     ''' + @ViewCurrencyCode + '''        AS metric_currency_name,
     ''' + @ViewCurrencyCode + '''        AS metric_currency_code,
 
     cal.calendar_date                    AS transaction_date,
 
-    CASE WHEN inv.Part_Of_HV_Staff = 1
-         THEN 1444282
-         ELSE inv.Parent_Investor_Id
-    END                                  AS investor_group_id,
-
-    CASE WHEN (grp.Part_Of_HV_Staff = 1 AND grp.investor_name_id <> -1)
-         THEN ''(Restricted)''
-         ELSE grp.Short_Name
-    END                                  AS investor_group_name,
-
-    inv.Short_Name                       AS investor_name,
-    inv.Address_Region                   AS investor_region,
-
-    it.Short_Name                        AS investor_type_name,
+    -- Company & Investment Info
+    c.Short_Name                         AS company_name,
+    inv.investment_year                  AS investment_year,
+    ind.industry_description             AS industry,
+    ind.industry_broad_code              AS industry_broad,
+    ind.industry_category_code           AS industry_category,
+    
     fund.Short_Name                      AS fund_name,
     fund.AIV_fund_group_id               AS aiv_fund_group_id,
     fund2.Type                           AS fund_type,
 
     jc.name                              AS join_currency_name,
-    
-    -- Company Information (if available)
-    c.Short_Name                         AS company_name,
-    c.Geography_Broad                    AS company_geo_broad,
-    c.Industry_Broad                     AS company_industry_broad,
 
     -- FX CONVERSION APPLIED HERE: Source -> Fund -> View
     SUM(f.amount * ISNULL(xr1.Rate, 1) * ISNULL(xr2.Rate, 1))   AS aggregated_amount
 
-FROM edw.fact_investor_transactions f
-JOIN investor_lookup il
-  ON f.investor_name_id = il.edw_key
-JOIN edw.dim_investor inv
-  ON f.investor_name_id = inv.investor_name_id
+FROM edw.fact_company_valuation f
 JOIN edw.dim_fund fund
   ON f.fund_id = fund.fund_id
 LEFT JOIN edw.dim_fund fund2
   ON f.fund_id = fund2.fund_id
 LEFT JOIN edw.calendar cal
   ON f.date_id = cal.date_id
-LEFT JOIN edw.dim_investor_type it
-  ON it.investor_type = f.investor_type
 LEFT JOIN edw.currency jc
-  ON f.fund_currency_id = jc.Currency_Id
-LEFT JOIN edw.dim_investor grp
-  ON (CASE WHEN inv.Part_Of_HV_Staff=1 THEN 1444282 ELSE inv.Parent_Investor_Id END) = grp.investor_name_id
-  
--- NEW JOIN: Company Dimension (Assumed)
--- NOTE: Please verify "edw.dim_company" and "direct_company_id" exist.
-LEFT JOIN edw.dim_company c
+  ON f.currency_id = jc.Currency_Id
+
+-- JOIN: Investment (Link Fund-Company)
+LEFT JOIN edw.investment inv
+  ON f.direct_company_id = inv.direct_company_id
+  AND f.fund_id = inv.fund_id
+
+-- JOIN: Company
+LEFT JOIN edw.company c
   ON f.direct_company_id = c.company_id
-  
--- FX JOIN 1: Source (f.currency_id) -> Fund (f.fund_currency_id)
+
+-- JOIN: Industry
+LEFT JOIN edw.industry ind
+  ON inv.industry_id = ind.industry_id
+
+-- FX JOIN 1
 LEFT JOIN edw.exchange_rates xr1
   ON xr1.From_Currency_ID = f.currency_id
-  AND xr1.To_Currency_ID = f.fund_currency_id
+  AND xr1.To_Currency_ID = f.fund_currency_id -- Assuming fund_currency_id exists in fact_company_valuation?
   AND xr1.Date = cal.calendar_date
 
--- FX JOIN 2: Fund (f.fund_currency_id) -> View (@ViewCurrencyID)
+-- FX JOIN 2
 LEFT JOIN edw.exchange_rates xr2
-  ON xr2.From_Currency_ID = f.fund_currency_id
+  ON xr2.From_Currency_ID = f.fund_currency_id -- verify column name
   AND xr2.To_Currency_ID = @ViewCurrencyID
   AND xr2.Date = cal.calendar_date
 
 WHERE
-    f.exclude_transaction = 0
-    AND f.date_id <> -1
+    f.date_id <> -1
     AND f.date_id BETWEEN @StartKey AND @EndKey
 ';
 
     -- Optional filters
     IF @FilterSourceCurrencyID IS NOT NULL
         SET @sql += N' AND f.currency_id = @FilterSourceCurrencyID ';
-
-    IF @InvestorGroupID IS NOT NULL
-        SET @sql += N' AND (CASE WHEN inv.Part_Of_HV_Staff=1 THEN 1444282 ELSE inv.Parent_Investor_Id END) = @InvestorGroupID ';
 
     IF @AIVFundGroupID IS NOT NULL
         SET @sql += N' AND fund.AIV_fund_group_id = @AIVFundGroupID ';
@@ -152,41 +136,36 @@ WHERE
     SET @sql += N'
     AND NOT EXISTS (SELECT 1 FROM fundtype_exclude fe WHERE fe.fund_type = fund2.Type)';
     
-    -- New Company Filters
-    IF @CompExpGeoBroad IS NOT NULL SET @sql += N' AND c.Geography_Broad = @CompExpGeoBroad ';
-    IF @CompExpGeoCountry IS NOT NULL SET @sql += N' AND c.Geography_Country = @CompExpGeoCountry ';
-    IF @CompExpIndId IS NOT NULL SET @sql += N' AND c.Industry_ID = @CompExpIndId ';
-    IF @CompExpIndBroad IS NOT NULL SET @sql += N' AND c.Industry_Broad = @CompExpIndBroad ';
-    IF @CompExpIndCategory IS NOT NULL SET @sql += N' AND c.Industry_Category = @CompExpIndCategory ';
-    IF @CompExpInvType IS NOT NULL SET @sql += N' AND c.Investment_Type = @CompExpInvType ';
-    IF @CompExpInvYear IS NOT NULL SET @sql += N' AND c.Investment_Year = @CompExpInvYear ';
-    IF @CompExpIsPublic IS NOT NULL SET @sql += N' AND c.Is_Public = @CompExpIsPublic ';
-    IF @CompExpStage IS NOT NULL SET @sql += N' AND c.Stage = @CompExpStage ';
-    IF @CompExpStageBroad IS NOT NULL SET @sql += N' AND c.Stage_Broad = @CompExpStageBroad ';
+    -- New Company Filters (Mapped to verified tables)
+    IF @CompExpGeoBroad IS NOT NULL SET @sql += N' AND f.company_geography_code = @CompExpGeoBroad '; -- Using fact table col per screenshot
+    IF @CompExpGeoCountry IS NOT NULL SET @sql += N' AND inv.country_id = @CompExpGeoCountry '; -- From Investment table
+    IF @CompExpIndId IS NOT NULL SET @sql += N' AND ind.industry_id = @CompExpIndId ';
+    IF @CompExpIndBroad IS NOT NULL SET @sql += N' AND ind.industry_broad_code = @CompExpIndBroad ';
+    IF @CompExpIndCategory IS NOT NULL SET @sql += N' AND ind.industry_category_code = @CompExpIndCategory ';
+    -- IF @CompExpInvType IS NOT NULL SET @sql += N' AND c.Investment_Type = @CompExpInvType '; -- Not clear in images, commenting out
+    IF @CompExpInvYear IS NOT NULL SET @sql += N' AND inv.investment_year = @CompExpInvYear ';
+    -- IF @CompExpIsPublic IS NOT NULL SET @sql += N' AND c.Is_Public = @CompExpIsPublic '; -- Not clear, commenting out
+    IF @CompExpStage IS NOT NULL SET @sql += N' AND inv.stage_code = @CompExpStage ';
+    IF @CompExpStageBroad IS NOT NULL SET @sql += N' AND inv.broad_stage = @CompExpStageBroad ';
 
     SET @sql += N'
 GROUP BY
     f.Metric_ID,
-    f.investor_name_id,
     cal.calendar_date,
-    CASE WHEN inv.Part_Of_HV_Staff=1 THEN 1444282 ELSE inv.Parent_Investor_Id END,
-    CASE WHEN (grp.Part_Of_HV_Staff=1 AND grp.investor_name_id <> -1) THEN ''(Restricted)'' ELSE grp.Short_Name END,
-    inv.Address_Region,
-    inv.Short_Name,
-    it.Short_Name,
+    c.Short_Name,
+    inv.investment_year,
+    ind.industry_description,
+    ind.industry_broad_code,
+    ind.industry_category_code,
     fund.Short_Name,
     fund.AIV_fund_group_id,
     fund2.Type,
-    jc.name,
-    c.Short_Name,
-    c.Geography_Broad,
-    c.Industry_Broad
+    jc.name
 ORDER BY ';
 
     SET @sql += CASE @OrderBy
         WHEN 'date'     THEN N' cal.calendar_date '
         WHEN 'fund'     THEN N' fund.Short_Name '
-        WHEN 'investor' THEN N' inv.Short_Name '
         WHEN 'amount'   THEN N' SUM(f.amount * ISNULL(xr1.Rate, 1) * ISNULL(xr2.Rate, 1)) ' 
         ELSE                 N' cal.calendar_date '
     END + N' DESC;';
